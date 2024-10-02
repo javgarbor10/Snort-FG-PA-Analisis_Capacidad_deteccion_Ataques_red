31e4b14cdf0eed5cfa9269acd4888e4acc37be63
javgarbor10 <163449359+javgarbor10@users.noreply.github.com>
2024-10-02 11:57:52 +0200

    Update tranalyzer.sh

--- Soporte/Otros/tranalyzer.sh
+++ Soporte/Otros/tranalyzer.sh
@@ -1,3 +1,11 @@
+# ESPECIFICACIONES SISTEMA OPERATIVO EN EL QUE SE HA HECHO USO DE TRANALYZER 
+#
+#  Distributor ID: Ubuntu
+#  Description: Ubuntu 22.04.4 LTS
+#  Release: 22.04
+#  Codename: jammy
+#
+
 cd /tmp
 wget https://tranalyzer.com/download/tranalyzer/tranalyzer2-0.9.3lmw1.tar.gz
 tar xzf tranalyzer2-0.9.3lmw1.tar.gz
