a45a959e65da9dc281dcbc0365919c6a1fedc34e
javgarbor10 <163449359+javgarbor10@users.noreply.github.com>
2024-10-02 11:48:21 +0200

    Create tranalyzer.sh

---
+++ Soporte/Otros/tranalyzer.sh
@@ -0,0 +1,6 @@
+cd /tmp
+wget https://tranalyzer.com/download/tranalyzer/tranalyzer2-0.9.3lmw1.tar.gz
+tar xzf tranalyzer2-0.9.3lmw1.tar.gz
+cd tranalyzer2-0.9.3lmw1
+./setup.sh -T
+sudo ./install.sh all
